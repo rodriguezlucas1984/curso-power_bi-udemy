6cfa9f9b34f75ec33418c900a0e9b856be2efcbc
Lucas Rodriguez <rodriguezlucasjavier1984@gmail.com>
2025-10-06 10:26:30 -0300

    Proyecto Indicadores mundiales

--- inicializar_origenes.bat
+++ inicializar_origenes.bat
@@ -5,9 +5,11 @@ set mypath=%cd%
 
 REM Origenes proyectos
 set origen_ventas="01-ventas-videojuegos\reporte\proyecto-ventas-videojuegos.SemanticModel\definition\expressions.tmdl"
+set origen_indicadores="02-indicadores-mundiales\reporte\proyecto-indicadores-mundiales.SemanticModel\definition\expressions.tmdl"
 
 REM Reemplazar en los archivos de origen
 powershell -Command "(Get-Content %origen_ventas%) -replace '##repo', '%mypath%' | Set-Content %origen_ventas%"
+powershell -Command "(Get-Content %origen_indicadores%) -replace '##repo', '%mypath%' | Set-Content %origen_indicadores%"
 
 echo Origenes inicializados.
 
